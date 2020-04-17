84c95a3bc734138619d191e5705a0dccd53b48fe
srinivasraosamudrala <srinivasrao.samudrala@users.noreply.github.com>
2020-04-17 01:18:30 -0700

    vm issue fix 2

--- Frontend/src/Utils/constants.js
+++ Frontend/src/Utils/constants.js
@@ -1,4 +1,5 @@
 export  const environment = {          
-   baseUrl : "http://localhost:3001"             
+   // baseUrl : "http://localhost:3001"             
 //  baseUrl : "http://3.21.124.167:3001"
+      baseUrl : "http://35.202.175.63:3001"
    }
